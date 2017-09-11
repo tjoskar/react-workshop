fd2c7b8a3dab2f3d0c2b56ba892f41db8ccd4f9d
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-09-11 20:30:58 +0200

    🎨  Make it ready for typescript

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import { TodoForm } from './todo-form';
 import { TodoList } from './todo-list';
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { TodoApp } from './app.jsx';
+import { TodoApp } from './app';
 
 ReactDOM.render(
   <TodoApp />,
